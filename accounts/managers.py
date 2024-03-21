b83e2fd7448b424ab62db2e5999e0a3236b45e41
Getonto3 <getontomoses18@gmail.com>
2024-03-21 18:02:00 +0300

    feat: add username as required field in the user model

--- accounts/managers.py
+++ accounts/managers.py
@@ -7,18 +7,18 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
     """Custom user manager using email as unique ID for auth."""
 
-    def create_user(self, email, password, **extra_fields):
-        """Create and save a user with the given email and password."""
+    def create_user(self, email, username, password, **extra_fields):
+        """Create and save a user with the given email username and password."""
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username,**extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+    def create_superuser(self, email, username, password, **extra_fields):
+        """Create and save a SuperUser with the given email username and password."""
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -27,4 +27,6 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
 
-        return self.create_user(email=email, password=password, **extra_fields)
+        return self.create_user(
+            email=email, username=username, password=password, **extra_fields
+        )
