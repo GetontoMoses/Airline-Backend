599b873847a5ca2cabc172c4316c6dcc66eab5da
Getonto3 <getontomoses18@gmail.com>
2024-03-21 18:05:28 +0300

    feat: student views

--- Student/views.py
+++ Student/views.py
@@ -1,1 +1,16 @@
 """Student views."""
+
+from rest_framework import generics
+
+from django.contrib.auth import get_user_model
+
+from .serializers import StudentSerializer
+
+User = get_user_model()
+
+
+class StudentSignUpView(generics.ListCreateAPIView):
+    """Create a new student."""
+
+    queryset = User.objects.all()
+    serializer_class = StudentSerializer
