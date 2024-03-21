9e14212fce51ee0e8159aa7045ac905224d9a488
Getonto3 <getontomoses18@gmail.com>
2024-03-21 18:05:00 +0300

    feat: Student urls

---
+++ Student/urls.py
@@ -0,0 +1,8 @@
+"""urls for the student app."""
+
+from django.urls import path
+from .views import StudentSignUpView
+
+urlpatterns = [
+    path("signup/", StudentSignUpView.as_view(), name="student_signup"),
+]
