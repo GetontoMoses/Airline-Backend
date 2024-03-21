63dbbe82890e28477137e9facb265f5060518070
Getonto3 <getontomoses18@gmail.com>
2024-03-21 22:05:02 +0300

    feat: user login view

--- Student/views.py
+++ Student/views.py
@@ -1,7 +1,9 @@
 """Student views."""
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 
 from .serializers import StudentSerializer
 
@@ -13,3 +15,22 @@ class StudentSignUpView(generics.ListCreateAPIView):
 
     queryset = User.objects.all()
     serializer_class = StudentSerializer
+
+
+class UserLoginView(generics.ListCreateAPIView):
+    """View for user login."""
+
+    def post(self, request):
+        """Handle user login."""
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+            # User authentication successful, generate JWT token
+            access_token = AccessToken.for_user(user)
+            return Response({"access_token": str(access_token)})
+        else:
+            # User authentication failed
+            return Response({"error": "Invalid credentials"}, status=401)
