52f91247d308b09c1dcf7e60a7d071b0f28e7cc9
Getonto3 <getontomoses18@gmail.com>
2024-03-21 18:03:54 +0300

    feat: Student serializer

---
+++ Student/serializers.py
@@ -0,0 +1,30 @@
+"""Student serialzers."""
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    """Student serializer."""
+
+    class Meta:
+        """Meta class for StudentSerializer."""
+
+        model = User
+        fields = ["username", "email", "password", "confirm_password"]
+
+    def validate(self, data):
+        """Validate the data."""
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError("The passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        """Create a new user."""
+        validated_data.pop("confirm_password", None)
+        user = User.objects.create_user(
+            username=validated_data["username"], email=validated_data["email"], password=validated_data["password"]
+        )
+        return user
