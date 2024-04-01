693377bba82d4dedc26db9b30032ebd0fbea1ebe
Getonto3 <getontomoses18@gmail.com>
2024-04-01 17:15:52 +0300

    feat: upload model and migrations

--- Student/models.py
+++ Student/models.py
@@ -1,1 +1,16 @@
 """Accounts app models."""
+
+from django.db import models
+
+
+class upload(models.Model):
+    """Upload model."""
+
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to="uploads/", blank=True, null=True)
+    year = models.CharField(max_length=100)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return name of the file."""
+        return self.name
