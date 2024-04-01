3d32873bf85e6e673e85fb467169d225f142390e
Getonto3 <getontomoses18@gmail.com>
2024-04-01 17:15:22 +0300

    feat: upload urls

--- Student/urls.py
+++ Student/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import path
 
-from .views import StudentSignUpView, UserLoginView
+from .views import StudentSignUpView, UploadView, UserLoginView
 
 urlpatterns = [
     path("signup/", StudentSignUpView.as_view(), name="student_signup"),
     path("login/", UserLoginView.as_view(), name="user_login"),
+    path("upload/", UploadView.as_view(), name="upload"),
 ]
