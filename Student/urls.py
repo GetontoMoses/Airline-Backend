c042a008fe63b7980f032942f4fdf0faf01a8e45
Getonto3 <getontomoses18@gmail.com>
2024-04-19 12:27:58 +0300

    feat: upload urls to return uploads based on users

--- Student/urls.py
+++ Student/urls.py
@@ -7,15 +7,15 @@ from .views import (
     UploadView,
     UserLoginView,
     UploadSearchAPIView,
-    UserProfileView,
-    MyUploads,
+    MyUploadsList,
+    MyUploadsCRUD,
 )
 
 urlpatterns = [
     path("signup/", StudentSignUpView.as_view(), name="student_signup"),
     path("login/", UserLoginView.as_view(), name="user_login"),
-    path("profile/<str:email>", UserProfileView.as_view(), name="user_profile"),
     path("upload/", UploadView.as_view(), name="upload"),
     path("search/", UploadSearchAPIView.as_view(), name="search"),
-    path("myuploads/<int:pk>", MyUploads.as_view(), name="myuploads"),
+    path("myuploads/<int:user>/", MyUploadsList.as_view(), name="myuploads"),
+    path("myuploadscrud/<int:pk>/", MyUploadsCRUD.as_view(), name="myuploads"),
 ]
